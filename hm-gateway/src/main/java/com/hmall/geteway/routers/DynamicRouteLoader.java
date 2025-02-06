783815d21c2bfa20cdf647d04540db4a1f8f89a1
Yang Zhao <joeyzy213@gmail.com>
2025-02-06 02:41:02 -0800

    configuration management for cart and gateway service, including shared-configuration for database, hot update and dynamic routing.

---
+++ hm-gateway/src/main/java/com/hmall/geteway/routers/DynamicRouteLoader.java
@@ -0,0 +1,78 @@
+package com.hmall.geteway.routers;
+
+import cn.hutool.json.JSONUtil;
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.nacos.api.config.listener.Listener;
+import com.alibaba.nacos.api.exception.NacosException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionWriter;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Executor;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DynamicRouteLoader {
+
+    private final NacosConfigManager nacosConfigManager;
+
+    private final RouteDefinitionWriter writer;
+
+    @Value("${hm.route.dataId}")
+    private String dataId;
+
+    @Value("${hm.route.group}")
+    private String group;
+
+    private final Set<String> routesIds = new HashSet<>();
+
+    @PostConstruct
+    public void initRouteConfigListener() throws NacosException {
+        // 1.项目启动，先拉取一次配置，并且添加配置监听器
+        String configInfo = nacosConfigManager.getConfigService()
+                .getConfigAndSignListener(dataId, group, 5000, new Listener() {
+                    @Override
+                    public Executor getExecutor() {
+                        return null;
+                    }
+
+                    @Override
+                    public void receiveConfigInfo(String configInfo) {
+                        // 2.监听到配置变更，需要去更新路由表
+                        updateConfigInfo(configInfo);
+
+                    }
+                });
+        // 3.第一次都获取到配置，更新到路由表
+        updateConfigInfo(configInfo);
+    }
+
+    public void updateConfigInfo(String configInfo){
+        log.debug("监听到路由配置信息：{}",configInfo);
+        // 1.解析配置信息，转换为RouteDefinition
+        List<RouteDefinition> routeDefinitions = JSONUtil.toList(configInfo, RouteDefinition.class);
+
+        // 2. 先删除旧的路由表
+        for(String routeId: routesIds){
+            writer.delete(Mono.just(routeId)).subscribe();
+        }
+        routesIds.clear();
+
+        // 3.更新路由表
+        for(RouteDefinition routeDefinition: routeDefinitions){
+            // 3.1 更新路由表
+            writer.save(Mono.just(routeDefinition)).subscribe();
+            // 3.2 记录路由id，下次更新是删除
+            routesIds.add(routeDefinition.getId());
+        }
+    }
+}
